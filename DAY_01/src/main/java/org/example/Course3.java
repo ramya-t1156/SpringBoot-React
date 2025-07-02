ae4f5b3b199e851272eb98f75ca9156027427056
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-02 21:18:15 +0530

    Add files via upload

---
+++ DAY_01/src/main/java/org/example/Course3.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Course3 {
+    public Course3() {
+        System.out.println("3 . MERN Stack");
+    }
+    public void enrollment(String name) {
+        System.out.println("You have enrolled successfully to MERN course");
+    }
+}
