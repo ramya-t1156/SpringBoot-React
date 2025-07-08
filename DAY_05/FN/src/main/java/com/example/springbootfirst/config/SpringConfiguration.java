3b947ea9264f10502add5422e781449f5dd9815c
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-08 13:56:38 +0530

    Add files via upload

---
+++ DAY_05/FN/src/main/java/com/example/springbootfirst/config/SpringConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.springbootfirst.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringConfiguration {
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.
+                csrf((CsrfConfigurer<HttpSecurity> csrf)-> csrf.disable())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+//        .httpBasic(Customizer.withDefaults())
+//          -- is what hides the default HTML login form and shows the browser pop-up login window instead.
+
+//        .formLogin(Customizer.withDefaults()); -- > for form
+        return http.build();
+    }
+}
