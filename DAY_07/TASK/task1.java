94326e6782f8053aafec7e5ad7428487db3b0588
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-10 22:13:06 +0530

    Update task1.java

--- DAY_07/TASK/task1.java
+++ DAY_07/TASK/task1.java
@@ -4,22 +4,34 @@
 // Controller
 @PutMapping("/{id}")
 @PreAuthorize("hasAnyRole('USER','ADMIN')")
-public String updateEmployee(@PathVariable int id, @RequestBody RegisterDetails reg){
+public String updateEmployee(@PathVariable int id, @RequestBody UserDetailsDto reg){
         return employeeService.updateEmployeeById(id, reg);
 }
 
 
 // Service
-public String updateEmployeeById(int id, RegisterDetails updatedData) {
-        RegisterDetails existing = regRepo.findById(id)
-                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+public String updateEmployeeById(int id, UserDetailsDto employee) {
+        Optional<RegisterDetails> existingEmp = regRepo.findById(id);
 
-        existing.setName(updatedData.getName());
-        existing.setUserName(updatedData.getUserName());
-        existing.setEmail(updatedData.getEmail());
-        existing.setPassword(passwordEncoder.encode(updatedData.getPassword()));
-        existing.setRoles(updatedData.getRoles());
+        if (existingEmp.isPresent()) {
+            RegisterDetails existingEmployee = existingEmp.get();
 
-        regRepo.save(existing);
-        return "Employee updated successfully";
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setPassword(employee.getPassword());
+            existingEmployee.setUserName(employee.getUserName());
+
+            Set<Roles> roles = new HashSet<>();
+            for (String roleName : employee.getRoleNames()) {
+                Roles role = roleRepo.findByRoleName(roleName)
+                        .orElseThrow(() -> new RuntimeException("Role not found"));
+                roles.add(role);
+            }
+            existingEmployee.setRoles(roles);
+
+            regRepo.save(existingEmployee);
+            return "Employee updated successfully";
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
 }
