0742ddcc97ee4f5dce177e8c34c7488b9c517df4
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-10 20:50:40 +0530

    Update task1.java

--- DAY_07/TASK/task1.java
+++ DAY_07/TASK/task1.java
@@ -1,5 +1,15 @@
 // TASK 1 - Update query by id
 
+
+// Controller
+@PutMapping("/{id}")
+@PreAuthorize("hasAnyRole('USER','ADMIN')")
+public String updateEmployee(@PathVariable int id, @RequestBody RegisterDetails reg){
+        return employeeService.updateEmployeeById(id, reg);
+}
+
+
+// Service
 public String updateEmployeeById(int id, RegisterDetails updatedData) {
         RegisterDetails existing = regRepo.findById(id)
                 .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
@@ -7,6 +17,9 @@ public String updateEmployeeById(int id, RegisterDetails updatedData) {
         existing.setName(updatedData.getName());
         existing.setUserName(updatedData.getUserName());
         existing.setEmail(updatedData.getEmail());
+        existing.setPassword(passwordEncoder.encode(updatedData.getPassword()));
+        existing.setRoles(updatedData.getRoles());
+
         regRepo.save(existing);
         return "Employee updated successfully";
 }
