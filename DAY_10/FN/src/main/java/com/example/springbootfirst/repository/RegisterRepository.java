dd123b8b8b14c68ad51e5b38ccc9c82b25dea38a
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-14 14:15:17 +0530

    Delete DAY_10/FN directory

--- DAY_10/FN/src/main/java/com/example/springbootfirst/repository/RegisterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootfirst.repository;
-
-import com.example.springbootfirst.models.RegisterDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterRepository extends JpaRepository<RegisterDetails, Integer> {
-}
