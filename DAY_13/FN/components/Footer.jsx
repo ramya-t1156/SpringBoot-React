a7c2e0eea376d221050114aaee038403a046fbb9
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-17 22:45:31 +0530

    Delete DAY_13/FN directory

--- DAY_13/FN/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="footer bg-dark text-white text-center py-3 mt-auto">
-            Created by Ramya T
-        </footer>
-    );
-};
-
-export default Footer;
